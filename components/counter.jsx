d316fef09a8876b598e37866bbc38c147f79ce65
SHIVA KASHYAP <93374924+Shiva-fighter@users.noreply.github.com>
2022-03-05 15:37:19 +0530

    Initial commit

---
+++ components/counter.jsx
@@ -0,0 +1,24 @@
+import {useState} from "react";
+
+export function Counter(){
+  
+  const [counter, setCounter] = useState(0);
+
+  const handleChange = (value)=>{
+    setCounter(counter + value);
+  }
+  return (
+    <>
+    <h3>Counter:{counter}</h3>
+    <button onClick = {()=>{
+                   handleChange(1)
+    }}>Add1</button>
+    <button onClick = {()=>{
+      if(counter>=1){
+        handleChange(- 1);
+      }
+      
+    }}>Reduce</button>
+    </>
+  )
+}
